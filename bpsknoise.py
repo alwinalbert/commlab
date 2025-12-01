05036ae2a52ad0f24aed9c4d4856df20108a020a
Alwiii <alwinalbert70@gmail.com>
2025-12-01 15:41:08 +0530

    noise ver added

--- bpsknoise.py
+++ bpsknoise.py
@@ -15,8 +15,10 @@ carrier = np.cos(2*np.pi*fc*t)
 samples_per_bit = int(fs*tb)
 symbol_wave = np.repeat(symbols, samples_per_bit)
 bpsk = symbol_wave*carrier
-
-detection = bpsk*carrier
+noise = np.random.randn(len(bpsk))
+bpsk_noisy = bpsk + noise
+    
+detection = bpsk_noisy*carrier
 
 detected_symbols = []
 for i in range(len(bits)):
@@ -34,34 +36,41 @@ for i in range(len(bits)):
 
 detected_symbols = np.array(detected_symbols)
 
-plt.figure(figsize=(12, 10))
+plt.figure(figsize=(10, 8))
 
-plt.subplot(5,1,1)
+plt.subplot(6,1,1)
 plt.title("input bits")
 plt.plot(t, np.repeat(bits, samples_per_bit), drawstyle='steps-post')
 plt.ylabel('Amplitude')
 plt.grid(True)
 
-plt.subplot(5,1,2)
+plt.subplot(6,1,2)
 plt.title("carrier wave (normal cosine wave)")
 plt.plot(t, carrier)
 plt.ylabel('Amplitude')
 plt.grid(True)
 
-plt.subplot(5,1,3)
+plt.subplot(6,1,3)
 plt.title("symbol wave")
 plt.plot(t, symbol_wave)
 plt.ylabel('Amplitude')
 plt.grid(True)
 
-plt.subplot(5,1,4)
+plt.subplot(6,1,4)
 plt.title("BPSK modulated signal")
 plt.plot(t, bpsk)
 plt.xlabel("Time (s)")
 plt.ylabel('Amplitude')
 plt.grid(True)
 
-plt.subplot(5,1,5)
+plt.subplot(6,1,5)
+plt.title("BPSK received signal with noise")            
+plt.plot(t, bpsk_noisy)
+plt.xlabel("Time (s)")
+plt.ylabel('Amplitude')
+plt.grid(True)
+
+plt.subplot(6,1,6)
 plt.title("input signal")
 plt.plot(t, detected_symbols.repeat(samples_per_bit), drawstyle='steps-post')
 plt.grid(True)
